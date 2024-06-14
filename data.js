0b2c684dcb40b2962d6527e7342e9d5e0e9ea40f
Hominglesi <creepersheep1010@gmail.com>
2024-06-14 02:33:34 +0200

    Add option to select games

--- data.js
+++ data.js
@@ -3,72 +3,84 @@ var data = `
     "nodes": [
         {
             "name": "Mutual Security Enhancing Simulation",
+            "game": "genshin",
             "category": "event",
             "startTime": "2024-06-07T02:00:00Z",
             "endTime": "2024-06-16T20:00:00Z"
         },
         {
             "name": "Spiral Abyss",
+            "game": "genshin",
             "category": "abyss",
             "startTime": "2024-05-31T22:00:00Z",
             "endTime": "2024-06-15T20:00:00Z"
         },
         {
             "name": "Spiral Abyss",
+            "game": "genshin",
             "category": "abyss",
             "startTime": "2024-06-15T20:00:00Z",
             "endTime": "2024-06-29T22:00:00Z"
         },
         {
             "name": "The Legend of the Galactic Baseballer",
+            "game": "starrail",
             "category": "event2",
             "startTime": "2024-05-31T22:00:00Z",
             "endTime": "2024-06-17T02:00:00Z"
         },
         {
             "name": "Memory of Chaos",
+            "game": "starrail",
             "category": "moc",
             "startTime": "2024-05-31T22:00:00Z",
             "endTime": "2024-06-18T21:00:00Z"
         },
         {
             "name": "Memory of Chaos",
+            "game": "starrail",
             "category": "moc2",
             "startTime": "2024-06-07T22:00:00Z",
             "endTime": "2024-06-29T22:00:00Z"
         },
         {
             "name": "Pure Fiction",
+            "game": "starrail",
             "category": "pf",
             "startTime": "2024-06-02T22:00:00Z",
             "endTime": "2024-06-29T22:00:00Z"
         },
         {
             "name": "Tower of Adversity",
+            "game": "wuwa",
             "category": "tower",
             "startTime": "2024-06-10T03:00:00Z",
             "endTime": "2024-06-24T03:00:00Z"
         },
         {
             "name": "Overdash Club",
+            "game": "wuwa",
             "category": "event3",
             "startTime": "2024-05-31T22:00:00Z",
             "endTime": "2024-06-17T03:00:00Z"
         },
         {
             "name": "Second Coming of Solaris",
+            "game": "wuwa",
             "category": "event4",
             "startTime": "2024-06-13T03:00:00Z",
             "endTime": "2024-06-27T03:00:00Z"
         },
         {
             "name": "Alloy Smelt",
+            "game": "wuwa",
             "category": "event5",
             "startTime": "2024-06-06T07:00:00Z",
             "endTime": "2024-06-27T03:00:00Z"
         },
         {
             "name": "Wuthering Exploration",
+            "game": "wuwa",
             "category": "event6",
             "startTime": "2024-06-13T07:00:00Z",
             "endTime": "2024-06-27T03:00:00Z"
--- script.js
+++ script.js
@@ -24,14 +24,11 @@ function createDateLineNow(){
     var dateLineNow = document.createElement("div");
     dateLineNow.classList += "dateLineNow";
     
-
     var dateObject = new Date();
 
     var offset = (dateObject.getDate() - 1) * 70;
     offset += (dateObject.getHours()) * 70 / 24;
     dateLineNow.style.left = offset + "px";
-
-    console.log(dateObject.getHours());
 
     container.appendChild(dateLineNow);
 }
@@ -43,6 +40,16 @@ function calculateDateOffset(date){
     offset += (dateObject.getHours()) * 70 / 24;
   
     return offset;
+}
+
+function getSelectedGames(){
+    var games = new Array();
+
+    if(document.getElementById("genshinCheckbox").checked) games.push("genshin");
+    if(document.getElementById("starrailCheckbox").checked) games.push("starrail");
+    if(document.getElementById("wuwaCheckbox").checked) games.push("wuwa");
+
+    return games;
 }
 
 
@@ -74,64 +81,89 @@ function createCalendarNode(row, name, start, end, color){
     container.appendChild(node);
 }
 
+function populateNodeDisplay(){
+    document.getElementById("NodeContainer").innerHTML = "";
+
+    createDateLines();
+
+    var games = getSelectedGames();
+
+    jsonData.nodes.forEach(element => {
+
+        if(games.indexOf(element.game) < 0) return;
+
+        var row = 0;
+        var color = "#ffcc66";
+
+        switch(element.category){
+            case "abyss": 
+                row = 0;
+                color = "#0066cc";
+                break;
+            case "event":
+                row = 1;
+                color = "#00ff99";
+                break;
+            case "event2":
+                row = 2;
+                color = "#9999ff";
+                break;
+            case "moc":
+                row = 3;
+                color = "#ff5050";
+                break;
+            case "moc2":
+                row = 4;
+                color = "#ff6666";
+                break;
+            case "pf":
+                row = 5;
+                color = "#ff66cc";
+                break;
+            case "tower":
+                row = 6;
+                color = "#ff9900";
+                break;
+            case "event3":
+                row = 7;
+                color = "#9999ff";
+                break;
+            case "event4":
+                row = 8;
+                color = "#00ffcc";
+                break;
+            case "event5":
+                row = 9;
+                color = "#cc3399";
+                break;
+            case "event6":
+                row = 10;
+                color = "#66ff33";
+                break;
+        }
+
+        createCalendarNode(row, element.name, element.startTime, element.endTime, color);
+    });
+
+    createDateLineNow();
+
+    localStorage.setItem("games", games.join("|"));
+}
+
+var localGames = localStorage.getItem("games")
+if(localGames != null){
+    var localGamesArray = localGames.split("|");
+
+    document.getElementById("genshinCheckbox").checked = localGamesArray.indexOf("genshin") >= 0;
+    document.getElementById("starrailCheckbox").checked = localGamesArray.indexOf("starrail") >= 0;
+    document.getElementById("wuwaCheckbox").checked = localGamesArray.indexOf("wuwa") >= 0;
+}
+
+document.getElementById("genshinCheckbox").addEventListener("click", (e) => populateNodeDisplay());
+document.getElementById("starrailCheckbox").addEventListener("click", (e) => populateNodeDisplay());
+document.getElementById("wuwaCheckbox").addEventListener("click", (e) => populateNodeDisplay());
+
 var jsonData = JSON.parse(data);
 
-createDateLines();
-
-jsonData.nodes.forEach(element => {
-
-    var row = 0;
-    var color = "#ffcc66";
-
-    switch(element.category){
-        case "abyss": 
-            row = 0;
-            color = "#0066cc";
-            break;
-        case "event":
-            row = 1;
-            color = "#00ff99";
-            break;
-        case "event2":
-            row = 2;
-            color = "#9999ff";
-            break;
-        case "moc":
-            row = 3;
-            color = "#ff5050";
-            break;
-        case "moc2":
-            row = 4;
-            color = "#ff6666";
-            break;
-        case "pf":
-            row = 5;
-            color = "#ff66cc";
-            break;
-        case "tower":
-            row = 6;
-            color = "#ff9900";
-            break;
-        case "event3":
-            row = 7;
-            color = "#9999ff";
-            break;
-        case "event4":
-            row = 8;
-            color = "#00ffcc";
-            break;
-        case "event5":
-            row = 9;
-            color = "#cc3399";
-            break;
-        case "event6":
-            row = 10;
-            color = "#66ff33";
-            break;
-    }
-
-    createCalendarNode(row, element.name, element.startTime, element.endTime, color);
-});
-
-createDateLineNow();
+populateNodeDisplay();
 
